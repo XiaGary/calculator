72a56feeaf875ba2069a8d56e29d0178315936ec
XiaGary <garyxia10@gmail.com>
2024-09-15 18:30:44 +1200

    Starting the HTML and the basic functions of the calculator project.

---
+++ script.js
@@ -0,0 +1,38 @@
+// Getting all the buttons from the calculator and assigning it to a variable
+const reset = document.querySelector('.reset');
+const plusMinus = document.querySelector('.plusMinus');
+const percentage = document.querySelector('.percentage');
+const divide = document.querySelector('.divide');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const multiply = document.querySelector('.multiply');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const subtract = document.querySelector('.subtract');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const addition = document.querySelector('.addition');
+const zero = document.querySelector('.zero');
+const decimal = document.querySelector('.decimal');
+const equal = document.querySelector('.equal');
+
+// Creating the function for each operator
+addition.addEventListener('click', add);
+function add(){
+
+}
+subtract.addEventListener('click', subtract);
+function subtract(){
+
+}
+multiply.addEventListener('click', multiply);
+function multiply(){
+
+}
+divide.addEventListener('click', divide);
+function divide(){
+
+}
