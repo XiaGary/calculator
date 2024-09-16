f40689a98fbb4d170253c0f64d8ef144e20e80f5
XiaGary <garyxia10@gmail.com>
2024-09-17 11:27:08 +1200

    Added eventListeners to each number of the calculator, and stores the number press to a variable.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Getting all the buttons from the calculator and assigning it to a variable
+//Getting all the buttons from the calculator and assigning it to a variable
 const reset = document.querySelector('.reset');
 const plusMinus = document.querySelector('.plusMinus');
 const percentage = document.querySelector('.percentage');
@@ -19,20 +19,62 @@ const zero = document.querySelector('.zero');
 const decimal = document.querySelector('.decimal');
 const equal = document.querySelector('.equal');
 
-// Creating the function for each operator
-addition.addEventListener('click', add);
-function add(){
+//Creating eventListeners for each number and storing the value
+zero.addEventListener('click', () => {
+    const numberZero = 0;
+    return numberZero;
+});
+one.addEventListener('click', () => {
+    const numberOne = 1;
+    return numberOne;
+});
+two.addEventListener('click', () => {
+    const numberTwo = 2;
+    return numberTwo;
+});
+three.addEventListener('click', () => {
+    const numberThree = 3;
+    return numberThree;
+});
+four.addEventListener('click', () => {
+    const numberFour = 4;
+    return numberFour;
+});
+five.addEventListener('click', () => {
+    const numberFive = 5;
+    return numberFive;
+});
+six.addEventListener('click', () => {
+    const numberSix = 6;
+    return numberSix;
+});
+seven.addEventListener('click', () => {
+    const numberSeven = 7;
+    return numberSeven;
+});
+eight.addEventListener('click', () => {
+    const numberEight = 8;
+    return numberEight;
+});
+nine.addEventListener('click', () => {
+    const numberNine = 9;
+    return numberNine;
+});
 
+//Creating the function for each math operator
+addition.addEventListener('click', add);
+function add(a,b){
+    return a + b;
 }
 subtract.addEventListener('click', subtract);
-function subtract(){
-
+function subtraction(a,b){
+    return a-b
 }
 multiply.addEventListener('click', multiply);
-function multiply(){
-
+function multiplication(a,b){
+    return a*b
 }
 divide.addEventListener('click', divide);
-function divide(){
-
+function division(a,b){
+    return a/b
 }
